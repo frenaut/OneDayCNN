0a3b2c40f7c381a0b39fbe99954390171c1b8ee4
frenaut <felix.renaut@gmail.com>
2018-12-02 15:55:55 +0100

    Added size to tensor, changed data to private

--- src/Tensor.cpp
+++ src/Tensor.cpp
@@ -1,8 +1,24 @@
 #include "../include/Tensor.hpp"
 
-Tensor::Tensor(int width, int height, int depth) {
-  assert(depth <= 3);
+Tensor::Tensor(int width, int height, int depth) : width_(width), height_(height), depth_(depth) {
+  assert(depth_ <= 3);
   for (int d = 0; d < depth; ++d) {
-      data[d] = Eigen::MatrixXf(height, width);
+      data_[d] = Eigen::MatrixXf(height, width);
   }
 } 
+
+Tensor::Tensor(const std::array<int, 3> & input_size) : Tensor(input_size[0], input_size[1], input_size[2]){}
+
+
+std::array<Eigen::MatrixXf, 3> Tensor::data() const {
+  return data_; 
+}
+
+std::array<int, 3> Tensor::size() const {
+  return std::array<int, 3> {{width_, height_, depth_}};
+}
+
+void Tensor::setData(const Eigen::MatrixXf & matrix, int depth){
+  assert(depth_ <= 3);
+  data_[depth] = matrix;
+}
